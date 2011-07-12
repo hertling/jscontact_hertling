8931346c70c950b387032d0c37493489859f2a3b
William Hertling <william.hertling@hp.com>
2011-07-12 09:20:57 -0700

    refactored to use ul/li

--- app/helpers/phone_numbers_helper.rb
+++ app/helpers/phone_numbers_helper.rb
@@ -1,6 +1,10 @@
 module PhoneNumbersHelper
   def print_numbers(numbers)
     #numbers.collect { |n| n.number }.join(", ")
-    numbers.collect(&:number).join(", ")
+    #numbers.collect(&:number).join(", ")
+    #"<ul><li>" + numbers.collect(&:number).join("</li><li>") + "</li></ul>"
+    content_tag :ul do
+      numbers.collect { |pn| content_tag :li, pn.number }.join.html_safe
+    end
   end
 end
--- spec/helpers/phone_numbers_helper_spec.rb
+++ spec/helpers/phone_numbers_helper_spec.rb
@@ -5,7 +5,7 @@ describe PhoneNumbersHelper do
     it "should output a comma separated list of phone numbers" do
       number_a=PhoneNumber.new(:number => "2223334444")
       number_b=PhoneNumber.new(:number => "5556667777")
-      print_numbers([number_a, number_b]).should == "2223334444, 5556667777"
+      print_numbers([number_a, number_b]).should == "<ul><li>2223334444</li><li>5556667777</li></ul>"
     end
     
     it "should not have a comma when there is only one number" do
