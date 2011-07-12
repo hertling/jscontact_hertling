a2859600ca63f040b678d207f173371bd050fa4c
William Hertling <william.hertling@hp.com>
2011-07-12 06:47:41 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jscontact::Application.config.secret_token = '80c79bfb26de83dd3f21dbb9f40d05746f55dad53583eac8a2d35e10a624695b24d89edc7fe6a3d188c5cb5998b9d36988081a5247056e30c8fc4942b6e7c450'
